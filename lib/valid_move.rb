e9ed4d8d90e9f7c61821eb16df51166c47252093
Chance Smith <chancesmith97@hotmail.com>
2020-02-08 01:13:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 def valid_move?(board, index)
   spot = nil
   if position_taken?(board, index) && !(index.between?(1, 9))
-    spot = false
+    spot = true
   else 
     spot = false
   end
